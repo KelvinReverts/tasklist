2d50c3291e5492d6d89646d3fd20761860c1edfd
KelvinReverts <kelvinrev2@hotmail.com>
2022-05-17 23:07:33 -0300

    version 1 system

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+// import {Container} from './App.styles'; to use <Container></Container>
+import { useState } from 'react';
+import * as C from './App.styles';
+import { Item } from './types/item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/addArea';
+
+const App = () =>{
+  const[list, setList] = useState<Item[]>([]);
+
+   const handleAddTask =(taskName: string)=>{
+      let newList =[...list];
+      newList.push({
+        id: list.length+1,
+        name: taskName,
+        done: false
+      });
+      setList(newList);
+   }
+  return(
+    <C.Container>
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        
+        <AddArea onEnter={handleAddTask}/>
+
+        {list.map((item,index)=>(
+          <ListItem key={index} item={item}/>
+        ))}
+      </C.Area>
+    </C.Container>
+  );
+}
+
+export default App;
